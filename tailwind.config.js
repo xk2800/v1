b9367999bfcbd3fe6095650fc0cd09935e409aa4
Xavier K <xavierkhew00@gmail.com>
2021-04-03 11:48:30 +0800

    import links folder

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+    darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {
+  
+          colors:{
+              primary:{
+                  'red': '#9F0710',
+              }
+          },
+  
+          fontFamily: {
+              main: [ 'Lexend Deca'],
+          },
+  
+  
+      },
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+  }
+  
