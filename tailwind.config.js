78ebee6e3963e33c67fdefa1579c7ed95786f7fe
Xavier K <xavierkhew00@gmail.com>
2021-08-07 19:37:26 +0800

    update to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,10 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-
             fontFamily: {
                 main: ['Lexend Deca'],
                 secondary: ['Roboto'],
             },
-            width: {
-                '120': '7.5rem',
-            },
-
-
         },
     },
     variants: {
