7867967199d4cd97c989ec2332dbb4b057c58858
Xavier K <khew.xavier@accenture.com>
2023-12-04 18:29:47 +0800

    add project archives

--- data/project.js
+++ data/project.js
@@ -1,9 +1,11 @@
 let project = [
   {
+    year: '',
     imagePath: "tcd-front-end.png",
     projectName: "Tech Career Days 2021",
     projectDescription: "A static site for Tech Career Days 2021, a job fair hosted by IT Society MMU Cyberjaya. Used to present relevant information for the event.",
     languagesUsed: "HTML, CSS, Bootstrap, Javascript",
+    featured: true,
     links: [
       {
         linkType: 2,
@@ -16,10 +18,12 @@ let project = [
     ]
   },
   {
+    year: '',
     imagePath: "tcd-resume.png",
     projectName: "Tech Career Days 2021 Resume Submission Portal",
-    projectDescription: "A dynamic site for Tech Career Days 2021, a job fair hosted by IT Society MMU Cyberjaya.  Used for job seekers to upload their resume where vetting is done by organisers and participating  companies are able to view the approved resumes.", 
+    projectDescription: "A dynamic site for Tech Career Days 2021, a job fair hosted by IT Society MMU Cyberjaya.  Used for job seekers to upload their resume where vetting is done by organisers and participating  companies are able to view the approved resumes.",
     languagesUsed: "HTML, CSS, Bootstrap, JavaScript, PHP, MySQL",
+    featured: true,
     links: [
       {
         linkType: 2,
@@ -28,10 +32,12 @@ let project = [
     ]
   },
   {
+    year: '',
     imagePath: "links-site.png",
     projectName: "Linktree Alternative",
-    projectDescription: "My own rework and customised approach to Linktree.", 
+    projectDescription: "My own rework and customised approach to Linktree.",
     languagesUsed: "Next JS, Tailwind CSS",
+    featured: true,
     links: [
       {
         linkType: 2,
@@ -44,13 +50,146 @@ let project = [
     ]
   },
   {
+    year: '',
     imagePath: "fyp.png",
     projectName: "REXFoodipedia",
-    projectDescription: "An F&B e-commerce platform with full function features for customers, staff and owners views. A project of 3, lead by me.", 
+    projectDescription: "An F&B e-commerce platform with full function features for customers, staff and owners views. A project of 3, lead by me.",
     languagesUsed: "HTML, CSS, Bootstrap, JavaScript, PHP, MySQL",
+    featured: true,
     links: [
-      
+
     ]
   },
-  
+  {
+    year: '2021',
+    imagePath: "",
+    projectName: "Desaru Coast",
+    projectDescription: "Resort website revamp",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, Bootstrap 4, JavaScript, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2021',
+    imagePath: "",
+    projectName: "Eve Holdings",
+    projectDescription: "Property developer website revamp",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, Bootstrap 4, JavaScript, JQuery, GSAP, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2022',
+    imagePath: "",
+    projectName: "RHB Cards",
+    projectDescription: "Website Revamp for cards product segment <br /> <div class='award-note'>Gold Award for Excellence in Relationship Marketing in Marketing Interactive, <a href='https://awards.marketing-interactive.com/marketing-excellence-my/2023-winners/' target='_blank' class='font-dark-hover'>Marketing Excellence Awards 2023</a></div>",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, SCSS, Bootstrap 4, JavaScript, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2022',
+    imagePath: "",
+    projectName: "TM Blog",
+    projectDescription: "Corporate blog for TM, built from scratch",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, SCSS, Bootstrap 4, JavaScript, JQuery, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2022',
+    imagePath: "",
+    projectName: "TM",
+    projectDescription: "Corporate website revamp",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, SCSS, Bootstrap 4, JavaScript, JQuery, Moment.js, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2022',
+    imagePath: "",
+    projectName: "TM Brand Centre",
+    projectDescription: "Corporate brand centre website for TM, built from scratch",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, SCSS, Bootstrap 4, JQuery, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2022',
+    imagePath: "",
+    projectName: "Unifi Business",
+    projectDescription: "Corporate website revamp",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, Bootstrap 4, JavaScript, JQuery, Slick",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2021-2023',
+    imagePath: "",
+    projectName: "Eucerin",
+    projectDescription: "Developing bi-weekly EDMs and maintaining existing website",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, CSS, Bootstrap 3, Javascript",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2023',
+    imagePath: "",
+    projectName: "Hartalega NGX",
+    projectDescription: "Digital clone of the physical factory in Sepang, Malaysia",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, Animate.css, SCSS, Bootstrap 5, GSAP, Javascript, JQuery, Three JS, Vite",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2023',
+    imagePath: "",
+    projectName: "Hartalega NGX",
+    projectDescription: "Digital clone of the physical factory in Sepang, Malaysia",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, Animate.css, SCSS, Bootstrap 5, GSAP, Javascript, JQuery, Three JS, Vite",
+    featured: false,
+    links: [
+
+    ]
+  },
+  {
+    year: '2023',
+    imagePath: "",
+    projectName: "Hartalega NGX",
+    projectDescription: "Digital clone of the physical factory in Sepang, Malaysia",
+    projectBuiltAt: "Accenture Song",
+    languagesUsed: "HTML, Animate.css, SCSS, Bootstrap 5, GSAP, Javascript, JQuery, Three JS, Vite",
+    featured: false,
+    links: [
+
+    ]
+  },
 ]
